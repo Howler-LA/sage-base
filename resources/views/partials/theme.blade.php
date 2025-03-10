23bdf7b0241eb6e8b4d121f66aca345dbb7f1348
Craig Fowler <craig@howler.studio>
2025-03-10 11:53:20 -0700

    colors

--- resources/views/partials/theme.blade.php
+++ resources/views/partials/theme.blade.php
@@ -3,15 +3,16 @@
 <style id="brand-styles">
   :root {
     /*Fonts*/
-    --eyebrow-font:       {!! $font['eyebrow'] !!};
-    --headline-font:      {!! $font['headline'] !!};
-    --body-font:          {!! $font['body'] !!};
-    --button-font:        {!! $font['button'] !!};
-    --key-font:           {!! $font['key'] !!};
+    --eyebrow-font:             {!! $font['eyebrow'] !!};
+    --headline-font:            {!! $font['headline'] !!};
+    --body-font:                {!! $font['body'] !!};
+    --button-font:              {!! $font['button'] !!};
+    --key-font:                 {!! $font['key'] !!};
     /*Brand Colors*/
-    --brand-background:         {!! $variables ? $brand['background_tw'] : $brand['background'] !!};
-    --brand-foreground:         {!! $variables ? $brand['foreground_tw'] : $brand['foreground'] !!};
-    --brand-accent-background:  {!! $variables ? $brand['accent_bg_tw'] : $brand['accent_bg'] !!};
-    --brand-accent-foreground:  {!! $variables ? $brand['accent_text_tw'] : $brand['accent_text'] !!};
+    --brand-background:         {!! $variables ? 'var(--color-' . $brand['background_tw'] . ')' : $brand['background'] !!};
+    --brand-subtle-background:  {!! $variables ? 'var(--color-' . $brand['background_subtle_tw'] . ')' : $brand['background_subtle'] !!};
+    --brand-foreground:         {!! $variables ? 'var(--color-' . $brand['foreground_tw'] . ')' : $brand['foreground'] !!};
+    --brand-accent-background:  {!! $variables ? 'var(--color-' . $brand['accent_bg_tw'] . ')' : $brand['accent_bg'] !!};
+    --brand-accent-foreground:  {!! $variables ? 'var(--color-' . $brand['accent_text_tw'] . ')' : $brand['accent_text'] !!};
   }
 </style>
