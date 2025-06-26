c1c2db7e79e1920dc456f382e0a6b0734dfa726a
Craig Fowler <craig@howler.studio>
2025-06-26 09:02:49 -0700

    added buttons

--- resources/views/partials/block__image_text.blade.php
+++ resources/views/partials/block__image_text.blade.php
@@ -12,18 +12,35 @@
       'xl:items-end' => $order == 'right',
     ])
   >
-    <x-lockup
-      @class([
-        'xl:w-full',
-        'p-lg xl:p-0',
-        'max-w-browser',
-        'mx-auto xl:mx-0',
-        'xl:max-w-[calc(calc(calc(var(--sizing-browser-width)*.5)-calc(var(--spacing-large)*2))*1px)]'
-      ])
-      eyebrow="{!! $content['eyebrow'] !!}"
-      headline="{!! $content['headline'] !!}"
-      copy="{!! $content['copy'] !!}"
-    />
+    <div class="flex flex-col gap-sm">
+      <x-lockup
+        @class([
+          'xl:w-full',
+          'p-lg xl:p-0',
+          'max-w-browser',
+          'mx-auto xl:mx-0',
+          'xl:max-w-[calc(calc(calc(var(--sizing-browser-width)*.5)-calc(var(--spacing-large)*2))*1px)]'
+        ])
+        eyebrow="{!! $content['eyebrow'] !!}"
+        headline="{!! $content['headline'] !!}"
+        copy="{!! $content['copy'] !!}"
+      />
+      @if($content['links'])
+        <x-button.group>
+          @foreach($content['links'] as $link)
+            <x-button
+              style="{{ $link['config']['style'] }}"
+              format="{{ $link['config']['format'] }}"
+              title="{!! $link['link']['title'] !!}"
+              target="{!! $link['link']['target'] !!}"
+              href="{!! $link['link']['url'] !!}"
+            >
+              {!! $link['link']['title'] !!}
+            </x-button>
+          @endforeach
+        </x-button.group>
+      @endif
+    </div>
   </div>
   <div 
     data-theme="{{ $config['media']['theme'] }}" 
@@ -42,6 +59,7 @@
       data-aos="fade-in"
       id="{!! $content['image'] !!}"
       @class([
+        'w-full h-full',
         'object-cover',
         // 'w-full h-auto rounded-lg' => $width != 'full',
         // 'absolute inset-0 w-full h-full' => $width == 'full',
