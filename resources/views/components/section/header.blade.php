1a59caba6192e0a42d123694837da98a6519c963
Craig Fowler <craig@howler.studio>
2025-07-03 08:11:21 -0700

    header fix

--- resources/views/components/section/header.blade.php
+++ resources/views/components/section/header.blade.php
@@ -1,3 +1,3 @@
-<x-container {{ $attributes->twMerge([$class, 'flex flex-col items-center justify-center gap-small text-center relative']) }}>
+<x-container {{ $attributes->twMerge(['flex flex-col items-center justify-center gap-small text-center relative']) }}>
 	{{ $slot }}
 </x-container>
