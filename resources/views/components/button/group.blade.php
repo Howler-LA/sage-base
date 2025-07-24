9befd325091f9c5c122715f7d524fd09e6d7c80b
Craig Fowler <craig@howler.studio>
2025-07-24 08:38:14 -0700

    YJC-45

--- resources/views/components/button/group.blade.php
+++ resources/views/components/button/group.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->twMerge(['flex flex-col xl:flex-row gap-2 xl:items-center']) }}>
+<div {{ $attributes->twMerge(['flex flex-col xl:flex-row p-px gap-2 xl:items-center']) }}>
 	{{ $slot }}
 </div>
