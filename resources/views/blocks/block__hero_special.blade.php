ee95757670670ea1af1421e99fd583a0650a803a
Craig Fowler <craig@howler.studio>
2025-07-24 13:23:40 -0700

    hero

--- resources/views/blocks/block__hero_special.blade.php
+++ resources/views/blocks/block__hero_special.blade.php
@@ -30,6 +30,21 @@
           />
         </div>
         <x-body :message="$content['copy']" />
+        @if($content['links'])
+          <x-card.footer>
+            <x-button.group>
+              @foreach($content['links'] as $link)
+                <x-button 
+                  variant="{{ $loop->iteration == 1 ? 'primary' : 'outline' }}"
+                  label="{{ $link['link']['title'] }}"
+                  title="{{ $link['link']['title'] }}"
+                  href="{{ $link['link']['url'] }}"
+                  target="{{ $link['link']['target'] }}"
+                />
+              @endforeach
+            </x-button.group>
+          </x-card.footer>
+        @endif
       </div>
     </div>
     <div class="">
