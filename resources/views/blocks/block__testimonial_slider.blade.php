ca452ec8d5c211520738d8f7888e1a40d039d90a
Craig Fowler <craig@howler.studio>
2025-07-24 08:36:28 -0700

    YJC-46

--- resources/views/blocks/block__testimonial_slider.blade.php
+++ resources/views/blocks/block__testimonial_slider.blade.php
@@ -38,7 +38,12 @@
         <div class="swiper-wrapper">
           @foreach(get_sub_field('cards') as $card)
             <div class="swiper-slide">
-              <div data-theme="Black" class="grid grid-cols-1 xl:grid-cols-2">
+              <div 
+                data-theme="Black" 
+                @class([
+                  'grid grid-cols-1 xl:grid-cols-2' => $card['image']
+                ])
+              >
                 <div class="p-large bg-background text-foreground">
                   <div class="size-full flex flex-col items-center justify-center text-center space-y-med">
                     <div class="space-y-zero">
