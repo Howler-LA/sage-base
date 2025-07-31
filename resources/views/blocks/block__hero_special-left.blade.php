e8cb394508c77064314c523aab8ab69d357635ba
Craig Fowler <craig@howler.studio>
2025-07-31 10:40:03 -0700

    left

--- resources/views/blocks/block__hero_special-left.blade.php
+++ resources/views/blocks/block__hero_special-left.blade.php
@@ -1,1 +1,85 @@
-Left!!
+<x-section data-theme="{{ $config['block']['themes'] }}">
+	<x-container>
+
+	</x-container>
+</x-section>
+
+{{-- <x-section
+  :padding="$align == 'center' ? '' : 'none'"
+  data-theme="{{ $config['block']['themes'] }}" 
+  @class([
+    'bg-background text-foreground',
+    'overflow-hidden',
+    'pb-0 overflow-hidden' => $content['image'],
+  ])
+>
+  <x-container
+    @class([
+      'grid grid-cols-1',
+      'gap-med xl:gap-med' => $align == 'center' && !$content['svg_headline'],
+      'gap-med xl:grid-cols-2 xl:gap-med' => $align != 'center'
+    ])
+  >
+    <div class="z-10">
+      <div
+        @class([
+          'flex flex-col justify-center h-full space-y-med',
+          'items-center text-balance' => $align == 'center' && !$content['svg_headline']
+        ])
+      >
+        <x-eyebrow :content="$content['eyebrow']" />
+        <div class="space-y-em">
+          <x-super-display 
+            @class([
+              'relative',
+              'items-center justify-center text-center xl:px-large' => $config['block']['align'] == 'center',
+              'items-start justify-start text-left' => $config['block']['align'] == 'left',
+              'items-end justify-end text-right' => $config['block']['align'] == 'right',
+              'sr-only' => $content['svg_headline']
+            ])
+            :message="$content['headline']"
+          />
+          <x-title 
+            :message="$content['subhead']" 
+          />
+        </div>
+        <x-body 
+          :message="$content['copy']" 
+          @class([
+            'max-w-prose mx-auto',
+            'text-center' => $align == 'center'
+          ])
+        />
+        @if($content['links'])
+          <div class="flex flex-col pt-em card-footer justify-end">
+            <x-button.group>
+              @foreach($content['links'] as $link)
+                <x-button 
+                  variant="{{ $loop->iteration == 1 ? 'primary' : 'outline' }}"
+                  label="{{ $link['link']['title'] }}"
+                  title="{{ $link['link']['title'] }}"
+                  href="{{ $link['link']['url'] }}"
+                  target="{{ $link['link']['target'] }}"
+                />
+              @endforeach
+            </x-button.group>
+          </div>
+        @endif
+      </div>
+    </div>
+    <div class="">
+      <div 
+        @class([
+          'xl:-mx-[12.5vw]' => $align != 'center'
+        ])
+      >
+        @image($content['image'],'large',[
+          'data-aos-delay' => '100', 
+          'data-aos'=>'fade-in', 
+          'class'=>'w-full h-auto block', 
+          'alt'=> $content['headline'] 
+        ])
+      </div>
+    </div>
+  </x-container>
+</x-section> --}}
