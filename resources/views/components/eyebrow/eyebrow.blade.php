bec5120cfa852d125def2af401b8f9a7b3ce99e8
Craig Fowler <craig@howler.studio>
2025-07-31 08:41:36 -0700

    eyebrow tweaks

--- resources/views/components/eyebrow/eyebrow.blade.php
+++ resources/views/components/eyebrow/eyebrow.blade.php
@@ -9,18 +9,26 @@
 })
 
 @if($slot->isNotEmpty() or $content != null)
-  <x-dynamic-component :component="$naked ? 'empty' : 'eyebrow.wrapper' ">
-    <div 
-      {{ $attributes->twMerge([
-        $class,
-        'font-eyebrow',
-        'font-extrabold',
-        'tracking-eyebrow',
-        'font-[var(--font-weight-eyebrow)]',
-        'uppercase',
-      ]) }}
-    >
-      {!! $content ?? $slot !!}
-    </div>
-  </x-dynamic-component>
+  <div class="flex items-center">
+    <x-dynamic-component class="flex-none" :component="$naked ? 'empty' : 'eyebrow.wrapper' ">
+      <div 
+        {{ $attributes->twMerge([
+          $class,
+          'whitespace-nowrap',
+          'font-eyebrow',
+          'font-extrabold',
+          'tracking-eyebrow',
+          'font-[var(--font-weight-eyebrow)]',
+          'uppercase',
+        ]) }}
+      >
+        {!! $content ?? $slot !!}
+      </div>
+    </x-dynamic-component>
+    @if(str_contains(get_bloginfo('wpurl'), 'initiatejustice'))
+      <svg class="h-full w-auto flex-none" viewBox="0 0 21 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18.9279 14.6261C20.4822 15.8271 20.4822 18.1729 18.928 19.3739L4.21161e-07 34L1.90735e-06 -9.6165e-07L18.9279 14.6261Z" fill="#FFCC02"/>
+      </svg>
+    @endif
+  </div>
 @endif
--- resources/views/components/eyebrow/wrapper.blade.php
+++ resources/views/components/eyebrow/wrapper.blade.php
@@ -1,8 +1,10 @@
 <div 
 	@class([
 		'bg-eyebrow-background text-eyebrow-foreground inline-flex',
-		'py-min px-em',
-		'rounded-tl-[30px] rounded-br-[30px] rounded-tr-[10px] rounded-bl-[10px]' => str_contains(get_bloginfo('wpurl'), 'youthjustice'),
+		'py-min',
+		'px-em',
+		'rounded-l pl-3 pr-0' => str_contains(get_bloginfo('wpurl'), 'initiatejustice'),
+		'px-em rounded-tl-[30px] rounded-br-[30px] rounded-tr-[10px] rounded-bl-[10px]' => str_contains(get_bloginfo('wpurl'), 'youthjustice'),
 	])
 >
 	{{ $slot }}
