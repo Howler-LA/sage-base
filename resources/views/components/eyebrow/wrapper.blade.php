71e0d0b2b0d28eab24875102d8bfe5fe42eb8212
Craig Fowler <craig@howler.studio>
2025-07-31 08:34:02 -0700

    eyebrow

--- resources/views/components/eyebrow/wrapper.blade.php
+++ resources/views/components/eyebrow/wrapper.blade.php
@@ -1,3 +1,9 @@
-<div class="py-2 px-3 rounded-[2px] bg-eyebrow-background text-eyebrow-foreground inline-flex">
+<div 
+	@class([
+		'bg-eyebrow-background text-eyebrow-foreground inline-flex',
+		'py-min px-em',
+		'rounded-tl-[30px] rounded-br-[30px] rounded-tr-[10px] rounded-bl-[10px]' => str_contains(get_bloginfo('wpurl'), 'youthjustice'),
+	])
+>
 	{{ $slot }}
 </div>
