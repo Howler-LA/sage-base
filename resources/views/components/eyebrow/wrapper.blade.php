e484d1dc0e0510a62df0970be2d29b6f6088a0e6
Craig Fowler <craig@howler.studio>
2025-08-06 16:47:38 -0700

    changes

--- resources/views/components/eyebrow/wrapper.blade.php
+++ resources/views/components/eyebrow/wrapper.blade.php
@@ -4,11 +4,7 @@
 		'py-min',
 		'px-em',
 		'border-eyebrow-border',
-<<<<<<< HEAD
 		'rounded-l pl-3 pr-0' => str_contains(get_bloginfo('wpurl'), 'initiatejustice'),
-=======
-		'border-l border-t border-b rounded-l pl-3 pr-0' => str_contains(get_bloginfo('wpurl'), 'initiatejustice'),
->>>>>>> 4565dbe843dfa8e302d4b04a5531ece8e7650cec
 		'border px-em rounded-tl-[30px] rounded-br-[30px] rounded-tr-[10px] rounded-bl-[10px]' => str_contains(get_bloginfo('wpurl'), 'youthjustice'),
 	])
 >
