62afa7c6443d4aa551076e9bc5f6de5741780d7e
Craig Fowler <craig@howler.studio>
2025-08-19 16:46:57 -0700

    silly line thing

--- resources/views/blocks/block__sticky_content.blade.php
+++ resources/views/blocks/block__sticky_content.blade.php
@@ -31,7 +31,7 @@
     <x-cols.col>
       @if($cards)
         @foreach($cards as $card)
-          <div class="p-section pb-0 last:pb-section bg-background">
+          <div class="p-section pb-px last:pb-section bg-background">
             <x-card
               data-theme="{{ $card['themes_card'] == null ? $config['block']['themes_cards'] : $card['themes_card'] }}"
               :variant="$type"
